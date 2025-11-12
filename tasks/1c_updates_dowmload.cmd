5345de87fb5fb020ba8ea446a211644beaf61c81
Глакдих О.С. <gladkikhos@42sol.ru>
2025-11-12 12:06:11 +0300

    Изменены конфигурации

--- tasks/1c_updates_dowmload.cmd
+++ tasks/1c_updates_dowmload.cmd
@@ -1,9 +1,9 @@
-call yard process "E:\workspace\oneScript\yard\examples\configurations\AccountingCorp30.json"
+call yard process "e:\workspace\oneScript\yard\examples\configurations\AccountingCorp30.json"
 
-call yard process "E:\workspace\oneScript\yard\examples\configurations\CRM32.json"
+call yard process "e:\workspace\oneScript\yard\examples\configurations\CPM32.json"
 
-call yard process "E:\workspace\oneScript\yard\examples\configurations\FoodCorp.json"
+call yard process "e:\workspace\oneScript\yard\examples\configurations\FoodCorp.json"
 
-call yard process "E:\workspace\oneScript\yard\examples\configurations\FitnessCorp.json"
+call yard process "e:\workspace\oneScript\yard\examples\configurations\FitnessCorp.json"
 
-call yard process "E:\workspace\oneScript\yard\examples\configurations\AccountingNPOCorp.json"
+call yard process "e:\workspace\oneScript\yard\examples\configurations\AccountingNPOCorp.json"
