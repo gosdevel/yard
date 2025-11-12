f15e2544e8f8e7c5b72819bbeb34e364d6e28fc5
Глакдих О.С. <gladkikhos@42sol.ru>
2025-11-12 11:57:08 +0300

    Добавлен файл запуска пакета

---
+++ tasks/1c_updates_dowmload.cmd
@@ -0,0 +1,9 @@
+call yard process "E:\workspace\oneScript\yard\examples\configurations\AccountingCorp30.json"
+
+call yard process "E:\workspace\oneScript\yard\examples\configurations\CRM32.json"
+
+call yard process "E:\workspace\oneScript\yard\examples\configurations\FoodCorp.json"
+
+call yard process "E:\workspace\oneScript\yard\examples\configurations\FitnessCorp.json"
+
+call yard process "E:\workspace\oneScript\yard\examples\configurations\AccountingNPOCorp.json"
